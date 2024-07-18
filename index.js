4732d3d69b4c828d5cb850c5259311f2b6ee8c78
Zack Njine Wachira <zackwachira45@gmail.com>
2024-07-18 08:58:10 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+  
+  
+  function updateDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
